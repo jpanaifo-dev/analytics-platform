52e79fbb47ed219b7b44d818bf203e8eaf010e47
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-21 23:45:32 -0500

    agregar imagen de fondo en la página de inicio de sesión

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,10 +21,11 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
+        <div className="absolute inset-0 bg-black/60 z-10" />
         <img
-          src="/placeholder.svg"
+          src="/images/bg-auth-webp"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
     </div>
